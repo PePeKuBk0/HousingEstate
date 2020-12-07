5a202e882d057595b1caea4592f1bf3851ac64f8
Jakub Venetianer <jakub.venetianer@gmail.com>
2020-12-07 15:11:50 +0100

    Flat.cs Added. int flatNumber, area, numberOfRooms.

---
+++ HousingEstate/Flat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingEstate
+{
+    class Flat
+    {
+        private int flatNumber;
+        private int area;
+        private int numberOfRooms;
+
+        public int FlatNumber { get; set; }
+        public int Area { get; set; }
+        public int NumberOfRooms { get; set; }
+
+        public Flat(int flatNumber, int area, int numberOfRooms)
+        {
+            this.flatNumber = flatNumber;
+            this.area = area;
+            this.numberOfRooms = numberOfRooms;
+        }
+        public override string ToString()
+        {
+            return $"FlatNumber:{this.flatNumber} \narea:{this.area} \nnumberOfRooms:{this.numberOfRooms}";
+        }
+            
+    }
+}
--- HousingEstate/Program.cs
+++ HousingEstate/Program.cs
@@ -8,6 +8,9 @@ namespace HousingEstate
         {
             Person A = new Person("Jakub","Venetianer",16);
             Console.WriteLine(A.ToString());
+            
+            Flat B = new Flat(24,100,4);
+            Console.WriteLine(B.ToString());
         }
     }
 }
