4a5a7cb08f0fc5b9c299312eb963ac98e522b6c6
Jakub Venetianer <jakub.venetianer@gmail.com>
2020-12-07 14:26:10 +0100

    Add project files.

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingEstate
+{
+    class Person
+    {
+        private string name;
+        private string surname;
+        private int age;
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surname, int age)
+       {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;    
+       }
+
+        public override string ToString()
+        {
+            return $"name:{this.name} \nsurname:{this.surname} \nage:{this.age} ";
+        }
+    }   
+}
---
+++ HousingEstate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HousingEstate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person A = new Person("Jakub","Venetianer",16);
+            Console.WriteLine(A.ToString());
+        }
+    }
+}
